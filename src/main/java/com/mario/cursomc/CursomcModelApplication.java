2114e6db30121fe4c82fa39af5ce8ead821be93b
Mario Camargos <mariocamargos@hotmail.com>
2021-03-05 13:39:00 -0300

    Iniciar o repository

---
+++ src/main/java/com/mario/cursomc/CursomcModelApplication.java
@@ -0,0 +1,13 @@
+package com.mario.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcModelApplication.class, args);
+	}
+
+}
